94d9a93df76f0a062003f0ab036dc6bf83a22393
Joaquín Losada <joaquin.losada@globant.com>
2016-03-27 21:12:11 -0300

    Empty Project

---
+++ src/main/java/com/globant/bootcamp/tae/App.java
@@ -0,0 +1,13 @@
+package com.globant.bootcamp.tae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
