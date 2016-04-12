d22d60b1efd43458e43348e6a7a1fa18e83d6642
Mauro Sanchez <msanch12@gmu.edu>
2016-04-12 03:58:33 -0300

    Ejercicio 05 Completo

--- src/main/java/com/globant/bootcamp/tae/App.java
+++ src/main/java/com/globant/bootcamp/tae/App.java
@@ -1,6 +1,7 @@
 package com.globant.bootcamp.tae; 
  
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -41,12 +42,15 @@ public class App
    
     @Test(description = "Ejercicio05") 
     public void ejercicio05() { 
- 
+    	
+    	
+    	driver.manage().window().maximize();
     	driver.get("http://amazon.com/"); 
     	WebElement element = driver.findElement(By.name("field-keywords"));
     	element.sendKeys("kindle");
     	driver.findElement(By.xpath("/html/body/div[2]/header/div/div[1]/div[3]/div/form/div[2]/div/input")).click();
     	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div[2]/div/div[4]/div[1]/div/ul/li[5]/div/div/div/div[2]/div[2]/a/h2")).click();
+    	Assert.assertTrue(driver.getTitle().toLowerCase().indexOf("kindle") >= 0, "no se encontro kindle en titulo");
     	driver.navigate().back();
     	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div[3]/div[2]/div/div[2]/ul[2]/li/span/a/span[2]")).click();
     	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[3]/div[3]/div[2]/div/div[2]/ul[3]/li[26]/a/span[1]")).click();
@@ -56,7 +60,6 @@ public class App
     	Assert.assertTrue(driver.findElements(By.xpath("/html/body/div[1]/div[1]/div[3]/div[2]/div/div[4]/div[1]/div/ul/li[4]/div/div/div/div[2]/div[2]/a/h2")).size() != 0, "no se encontraron al menos 4 resultados");    	
     	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[1]/div[1]/div/a[1]/span[1]")).click();
     	Assert.assertTrue(driver.getTitle() != "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", "titulo no correcto");
-
     } 
 
 } 
